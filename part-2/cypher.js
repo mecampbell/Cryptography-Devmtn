17420c00dc49cf00c391bb475858b4a1ced0afd3
Michael Campbell <michaelmichael323@hotmail.com>
2022-05-26 19:54:26 -0600

    finished

--- part-2/cypher.js
+++ part-2/cypher.js
@@ -1,1 +1,12 @@
-const str = 'I love cryptography';
+const encrypt = (str) => {
+    let newString = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+        if (newString.contains('a')) {
+
+        }
+    }
+    return newString;
+};
+
+console.log(encrypt('message'));
